e6b90257fdbd8449edb881589fa213adf0952e99
René Logala Modia <90214842+logmodia@users.noreply.github.com>
2021-12-14 10:10:04 +0100

    save

--- php/dbconnection.php
+++ php/dbconnection.php
@@ -2,17 +2,12 @@
 
 if (!isset($_ENV['HOST'])){
     include_once('var_environ.php');
-    define("HOST", $_ENV['HOST']);
-    define("DB", $_ENV['DB']);
-    define("PORT", $_ENV['PORT']);
-    define("LOGIN", $_ENV['LOGIN']);
-    define("PASSWORD", $_ENV['PASSWORD']);
 }
 
 try {
 
     // We create a new instance of the class PDO
-    $db = new PDO("mysql:host=".HOST.";dbname=".DB.";port=".PORT, LOGIN, PASSWORD);
+    $db = new PDO("mysql:host=".$_ENV['HOST'].";dbname=".$_ENV['DB'].";port=".$_ENV['DB_PORT'],  $_ENV['LOGIN'], $_ENV['PASSWORD']);
 
     //We want any issues to throw an exception with details, instead of a silence or a simple warning
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
