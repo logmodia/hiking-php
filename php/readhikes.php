3160283b60402acef03178193409b33c19518999
René Logala Modia <90214842+logmodia@users.noreply.github.com>
2021-12-14 11:41:13 +0100

    add footer page

--- php/readhikes.php
+++ php/readhikes.php
@@ -128,32 +128,8 @@
 
     </main>
 
-    <footer class="footer">
-      <section>
-        <ul class="footer__container">
-          <li class="footer__social-items">
-            <a href="#" class="footer__social-link" aria-label="facebook"
-              ><i class="bx bxl-facebook"></i
-            ></a>
-          </li>
-          <li class="footer__social-items">
-            <a href="#" class="footer__social-link" aria-label="twitter"
-              ><i class="bx bxl-twitter"></i
-            ></a>
-          </li>
-          <li class="footer__social-items">
-            <a href="#" class="footer__social-link" aria-label="instagram"
-              ><i class="bx bxl-instagram"></i
-            ></a>
-          </li>
-          <li class="footer__social-items">
-            <a href="#" class="footer__social-link" aria-label="youtube"
-              ><i class="bx bxl-youtube"></i
-            ></a>
-          </li>
-        </ul>
-      </section>
-    </footer>
+    <?php include_once("footer.php");?>
+
     <script src="./js/index.js"></script>
   </body>
 </html>
