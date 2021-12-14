9531658e23a56f76ea402a3e68183cf0bb7112c5
René Logala Modia <90214842+logmodia@users.noreply.github.com>
2021-12-14 12:35:13 +0100

    add logout feature

--- php/header.php
+++ php/header.php
@@ -1,7 +1,8 @@
 <header class="header">
     <nav class="header__nav">
-        <li class="header__nav__element">Contact us</li>
-        <li class="header__nav__element">Settings</li>
+        <li class="header__nav__element"><a href="#">Contact us</a></li>
+        <li class="header__nav__element"><a href="#">Settings</a></li>
+        <li class="header__nav__element"><a href="/php/logout.php?logout_status=<?php echo 'true'; ?>">Logout</a></li>
     </nav>
     <div class="header__containnerLogoTitle">
     
